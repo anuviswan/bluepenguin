00b91e2a18ff8da8ea4ed5a8c8974a13f3b6fed0
Anu Viswan <anu.viswan@gmail.com>
2023-11-11 19:20:39 +0530

    upload

--- Apps/src/BluePenguin.Catalogue.AzureFunction/InputFunctions.cs
+++ Apps/src/BluePenguin.Catalogue.AzureFunction/InputFunctions.cs
@@ -1,35 +1,59 @@
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BluePenguin.Catalogue.AzureFunction
 {
     public static class InputFunctions
     {
         [FunctionName("GetAllProducts")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> UploadProducts(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,
+            [Blob("productlist")] CloudBlobContainer blobContainer, 
+            TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
-
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
-
-            if (name == null)
+            MultipartMemoryStreamProvider stream;
+            try
             {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
+                var multipartTask = req.Content.ReadAsMultipartAsync();
+                if (!multipartTask.IsCompleted)
+                {
+                    multipartTask.RunSynchronously();
+                }
+                stream = multipartTask.Result;
+            }
+            catch (Exception)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Posted data was not multipart or read failed");
             }
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            string fileContent;
+            try
+            {
+                var httpContent = stream.Contents[0];
+                var contentTask = httpContent.ReadAsStringAsync();
+                if (!contentTask.IsCompleted)
+                {
+                    contentTask.RunSynchronously();
+                }
+                fileContent = contentTask.Result;
+            }
+            catch (Exception)
+            {
+                return req.CreateResponse(HttpStatusCode.InternalServerError, "Unable to read file stream");
+            }
+
+
+            await blobContainer.CreateIfNotExistsAsync();
+
+            var blob = blobContainer.GetBlockBlobReference($"productlist.xml");
+            await blob.UploadTextAsync(fileContent);
+            return req.CreateResponse(HttpStatusCode.Created, "Product List Uploaded"); ;
         }
     }
 }
