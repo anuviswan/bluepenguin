976c3dbe7bfb2d09a9d01cf022f8ea7885b24ea7
Anu Viswan <anu.viswan@gmail.com>
2023-11-25 17:02:35 +0530

    Updated Product Meta information in Azure Function

--- Apps/src/BluePenguin.Catalogue.AzureFunction/DTO/Products.cs
+++ Apps/src/BluePenguin.Catalogue.AzureFunction/DTO/Products.cs
@@ -27,6 +27,28 @@ namespace BluePenguin.Catalogue.AzureFunction.DTO
 
         [XmlElement(ElementName = "Collection")]
         public string Collection { get; set; }
+
+        [XmlElement(ElementName = "Code")]
+        public string Id { get; set; }
+
+        [XmlElement(ElementName = "Tags")]
+        public Tags Tags { get; set; }
+
+        [XmlElement(ElementName = "Size")]
+        public string Size { get; set; }
+
+        [XmlElement(ElementName = "MRP")]
+        public double MRP { get; set; }
+
+        [XmlElement(ElementName = "DiscountPrice")]
+        public double DiscountPrice { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Tags")]
+    public class Tags
+    {
+        [XmlElement(ElementName = "Tag")]
+        public List<string> Tag { get; set; }
     }
 
     [XmlRoot(ElementName = "Products")]
