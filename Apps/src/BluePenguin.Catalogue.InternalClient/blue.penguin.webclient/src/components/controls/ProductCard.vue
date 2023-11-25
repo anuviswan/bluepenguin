a4ff7eb676801c171b81f7028a0a8119d6e777c8
Anu Viswan <anu.viswan@gmail.com>
2023-11-25 21:45:06 +0530

    Working on Product Card

--- Apps/src/BluePenguin.Catalogue.InternalClient/blue.penguin.webclient/src/components/controls/ProductCard.vue
+++ Apps/src/BluePenguin.Catalogue.InternalClient/blue.penguin.webclient/src/components/controls/ProductCard.vue
@@ -1,62 +1,60 @@
 <template>
-   <v-card
-   prepend-icon="mdi-account"
-   :title = product.name
-   :subtitle = product.category
-    class="mx-auto my-12"
-    max-width="374"
-  >
-
-    <!-- <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img> -->
-
-
-    <v-card-text>
-
-      <div class="my-4 text-subtitle-1">
-        {{product.category}} , {{product.collection}}
-      </div>
-
-      <div>{{product.MRP}}</div>
-    </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-    <v-card-subtitle>MRP Rs.{{product.mrp}}</v-card-subtitle>
-    <v-card-subtitle>Discount Price Rs.{{product.discountPrice}}</v-card-subtitle>
-    <v-divider class="mx-4"></v-divider>
+  <v-card class="mx-auto my-12" max-width="374">
+    <v-card-item>
+      <v-card
+        :color="titleColor"
+        text-center
+        :title="product.name"
+        :subtitle="product.category"
+      >
+      </v-card>
+    </v-card-item>
+    <v-card-subtitle>MRP Rs.{{ product.mrp }}</v-card-subtitle>
+    <v-card-subtitle
+      >Discount Price Rs.{{ product.discountPrice }}</v-card-subtitle
+    >
     <v-card-text>
       <v-chip-group
         v-model="selection"
         active-class="deep-purple accent-4 white--text"
         column
       >
-      <div v-for="item in product.tags.Tag" :key="item.name">
-          <div  class="mx-4">
-            <v-chip>{{item}}</v-chip>
+        <div v-for="item in product.tags.Tag" :key="item.name">
+          <div class="mx-4">
+            <v-chip>{{ item }}</v-chip>
           </div>
         </div>
       </v-chip-group>
     </v-card-text>
 
     <v-card-actions>
-      <v-btn
-        color="deep-purple lighten-2"
-        text
-        
-      >
-        Details
-      </v-btn>
+      <v-btn color="deep-purple lighten-2" text> Details </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script setup lang="ts">
 import { IProduct } from "@/types/UserTypes";
+import {computed} from "vue"
 
 const props = defineProps<{
-    product : IProduct
+  product: IProduct;
 }>();
+
+const titleColor = computed(()=> {
+  switch (props.product.category) {
+    
+    case "CANVAS":
+      return "blue";
+    
+    case "CLOCK":
+      return "indigo";
+    
+    case "PENDANT":
+      return "teal";
+    default:
+      return "secondary";
+  }
+})
 
 </script>
