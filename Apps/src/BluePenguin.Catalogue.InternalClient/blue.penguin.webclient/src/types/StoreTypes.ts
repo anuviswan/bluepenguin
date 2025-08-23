ad6d2b98966f085e0127d31c2bcc36442a3d714a
Anu Viswan <anu.viswan@gmail.com>
2025-08-23 16:12:56 +0530

    removed obsolete app

--- Apps/src/BluePenguin.Catalogue.InternalClient/blue.penguin.webclient/src/types/StoreTypes.ts
+++
@@ -1,5 +0,0 @@
-// import { User } from "./UserTypes";
-
-// export interface LoggedInUser extends User{
-//     token : string 
-// } 
--- Apps/src/BluePenguin.Catalogue.InternalClient/blue.penguin.webclient/src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Dashboard />
-</template>
-
-<script lang="ts" setup>
-  import Dashboard from '@/components/Dashboard.vue'
-
-  console.log(import.meta.env.VITE_API_URL)
-</script>
