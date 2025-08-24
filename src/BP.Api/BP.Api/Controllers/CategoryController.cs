ca263148ff6c416d92f046607c8d47ad4a999a48
Anu Viswan <anu.viswan@gmail.com>
2025-08-24 18:32:45 +0530

    adding domain layer

---
+++ src/BP.Api/BP.Api/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BP.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoryController:BaseController
+{
+    public CategoryController(ILogger<CategoryController> logger):base(logger)
+    {
+            
+    }
+
+    [HttpPost]
+    [Route("create")]
+    public IActionResult CreateCategory()
+    {
+        Logger.LogInformation("Test endpoint hit");
+        return Ok("CategoryController is working!");
+    }
+}
