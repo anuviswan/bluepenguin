42704302d3b5d09fe4b2c3d6d75f88767968a908
Anu Viswan <anu.viswan@gmail.com>
2025-08-24 20:59:10 +0530

    register services

---
+++ src/BP.Api/BP.Api/ExtensionMethods/IServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace BP.Api.ExtensionMethods;
+
+public static class IServiceCollectionExtension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddTransient<BP.Application.Interfaces.Services.IProductService, BP.Application.Services.ProductService>();
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<BP.Domain.Repository.IProductRepository, BP.Infrastructure.Repositories.ProductRepository>();
+        return services;
+    }
+}
--- src/BP.Api/BP.Api/Program.cs
+++ src/BP.Api/BP.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using BP.Api.ExtensionMethods;
+
 namespace BP.Api;
 
 public class Program
@@ -13,6 +15,8 @@ public class Program
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+        builder.Services.AddApplicationServices();
+        builder.Services.AddRepositories();
 
         var app = builder.Build();
 
