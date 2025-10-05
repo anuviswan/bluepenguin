3194018afea5e2ba52cade99514da2270231e203
Anu Viswan <anu.viswan@gmail.com>
2025-10-05 05:47:43 +0530

    added collection service

--- src/BP.Admin.WebClient/src/types/ProductTypes.ts
+++ src/BP.Admin.WebClient/src/types/ProductTypes.ts
@@ -3,13 +3,10 @@ export interface KeyValuePair{
     Id:string;
     Name:string;
 }
-export interface Category extends KeyValuePair{
-}
+export interface Category extends KeyValuePair{}
 
-export interface Material extends KeyValuePair{
+export interface Material extends KeyValuePair{}
 
-}
+export interface Feature extends KeyValuePair{}
 
-export interface Feature extends KeyValuePair{
-
-}
+export interface Collection extends KeyValuePair{}
