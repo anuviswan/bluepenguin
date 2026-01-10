9a4e08ec8524b4273f11e5d20483426bcfd6eef3
Anu Viswan <anu.viswan@gmail.com>
2026-01-10 20:41:09 +0530

    rework on seed

--- src/BP.Api/BP.Api/Controllers/SeedController.cs
+++ src/BP.Api/BP.Api/Controllers/SeedController.cs
@@ -1,13 +1,11 @@
-﻿using BP.Application.Interfaces.Services;
-using Microsoft.AspNetCore.Mvc;
-using BP.Api.Contracts;
+﻿using BP.Api.Contracts;
+using BP.Application.Interfaces.Services;
 using BP.Application.Interfaces.SkuAttributes;
-using Microsoft.AspNetCore.Authorization;
 using BP.Shared.Types;
-using System.IO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Processing;
 
 namespace BP.Api.Controllers;
@@ -22,49 +20,45 @@ public class SeedController(IProductController productController, IProductImageS
     [Route("seed/execute")]
     public async Task<IActionResult> ExecuteSeed()
     {
-        var result = await SeedProducts();
-        return Ok(result);
+        await SeedProducts();
+        return Ok();
     }
 
-    private async Task<object?> SeedProducts()
+    private async Task SeedProducts()
     {
         var seeds = new List<CreateProductRequest>
         {
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Onam Floral Ring",
                 Price = 199.99,
                 Category = Category.RI.ToString(),
                 Material = Material.RS.ToString(),
-                FeatureCodes = new[] { Feature.FL.ToString() },
+                FeatureCodes = [Feature.FL.ToString()],
                 CollectionCode = Collection.ONM.ToString(),
                 YearCode = 2024,
                 SequenceCode = 1
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Christmas Mirror Pendant",
                 Price = 249.5,
                 Category = Category.PD.ToString(),
                 Material = Material.CY.ToString(),
-                FeatureCodes = new[] { Feature.MR.ToString(), Feature.PL.ToString() },
+                FeatureCodes = [Feature.MR.ToString(), Feature.PL.ToString()],
                 CollectionCode = Collection.CMS.ToString(),
                 YearCode = 2023,
                 SequenceCode = 1
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Ocean Sea Elements Necklace",
                 Price = 299.0,
                 Category = Category.NK.ToString(),
                 Material = Material.BD.ToString(),
-                FeatureCodes = new[] { Feature.SE.ToString(), Feature.ST.ToString() },
+                FeatureCodes = [Feature.SE.ToString(), Feature.ST.ToString()],
                 CollectionCode = Collection.OCN.ToString(),
                 YearCode = 2024,
                 SequenceCode = 2
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Nature Embedded Bracelet",
                 Price = 149.75,
                 Category = Category.BR.ToString(),
@@ -74,75 +68,68 @@ public class SeedController(IProductController productController, IProductImageS
                 YearCode = 2022,
                 SequenceCode = 1
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Traditional Glitter Earrings",
                 Price = 129.0,
                 Category = Category.ER.ToString(),
                 Material = Material.CY.ToString(),
-                FeatureCodes = new[] { Feature.GL.ToString(), Feature.SC.ToString() },
+                FeatureCodes = [Feature.GL.ToString(), Feature.SC.ToString()],
                 CollectionCode = Collection.TRD.ToString(),
                 YearCode = 2021,
                 SequenceCode = 3
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Spotlight Multi-color Ring",
                 Price = 219.99,
                 Category = Category.RI.ToString(),
                 Material = Material.BD.ToString(),
-                FeatureCodes = new[] { Feature.MC.ToString(), Feature.FD.ToString() },
+                FeatureCodes = [Feature.MC.ToString(), Feature.FD.ToString()],
                 CollectionCode = Collection.SLT.ToString(),
                 YearCode = 2024,
                 SequenceCode = 4
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Signature Pendant Metallic",
                 Price = 329.0,
                 Category = Category.PD.ToString(),
                 Material = Material.RS.ToString(),
-                FeatureCodes = new[] { Feature.MT.ToString(), Feature.PL.ToString() },
+                FeatureCodes = [Feature.MT.ToString(), Feature.PL.ToString()],
                 CollectionCode = Collection.SGN.ToString(),
                 YearCode = 2020,
                 SequenceCode = 2
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Vintage Framed Necklace",
                 Price = 279.5,
                 Category = Category.NK.ToString(),
                 Material = Material.CY.ToString(),
-                FeatureCodes = new[] { Feature.FR.ToString(), Feature.PS.ToString() },
+                FeatureCodes = [Feature.FR.ToString(), Feature.PS.ToString()],
                 CollectionCode = Collection.VIN.ToString(),
                 YearCode = 2019,
                 SequenceCode = 5
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Onam Custom Charm Bracelet",
                 Price = 159.0,
                 Category = Category.BR.ToString(),
                 Material = Material.BD.ToString(),
-                FeatureCodes = new[] { Feature.CH.ToString(), Feature.PR.ToString() },
+                FeatureCodes = [Feature.CH.ToString(), Feature.PR.ToString()],
                 CollectionCode = Collection.ONM.ToString(),
                 YearCode = 2024,
                 SequenceCode = 6
             },
-            new CreateProductRequest
-            {
+            new() {
                 Name = "Spotlight Waves Earrings",
                 Price = 139.99,
                 Category = Category.ER.ToString(),
                 Material = Material.RS.ToString(),
-                FeatureCodes = new[] { Feature.WV.ToString(), Feature.IN.ToString() },
+                FeatureCodes = [Feature.WV.ToString(), Feature.IN.ToString()],
                 CollectionCode = Collection.SLT.ToString(),
                 YearCode = 2023,
                 SequenceCode = 2
             }
         };
 
-        var created = new List<object?>();
         foreach (var seed in seeds)
         {
             try
@@ -151,13 +138,9 @@ public class SeedController(IProductController productController, IProductImageS
                 string? sku = null;
                 if (res is ObjectResult or)
                 {
-                    created.Add(or.Value);
                     sku = or.Value as string;
                 }
-                else
-                {
-                    created.Add(null);
-                }
+
 
                 if (!string.IsNullOrWhiteSpace(sku))
                 {
@@ -182,21 +165,18 @@ public class SeedController(IProductController productController, IProductImageS
                     try
                     {
                         var url = await ProductImageService.UploadAsync(fileUpload, true);
-                        created.Add(new { sku, imageUrl = url });
                     }
                     catch (Exception ex)
                     {
-                        created.Add(new { sku, uploadError = ex.Message });
+                        // Decide what to do if image upload fails
                     }
                 }
             }
             catch (Exception ex)
             {
-                created.Add(ex.Message);
+                // Decide what to do if product creation fails
             }
         }
-
-        return created;
     }
 
 }
