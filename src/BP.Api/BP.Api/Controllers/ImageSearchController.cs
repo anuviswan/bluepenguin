250c0f138868a2ba68dc763ce0bacb0e067fc081
Anu Viswan <anu.viswan@gmail.com>
2026-03-05 19:26:28 +0530

    starting with image search controller

---
+++ src/BP.Api/BP.Api/Controllers/ImageSearchController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BP.Api.Controllers
+{
+    public class ImageSearchController : Controller
+    {
+        [HttpPost]
+        [Route("indexall")]
+        public IActionResult IndexAll()
+        {
+            return View();
+        }
+    }
+}
